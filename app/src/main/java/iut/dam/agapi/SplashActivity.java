8784c34d1249cd737deaf12798f190c27245b453
BaZz Ychnightder <pierreychnightder561@gmail.com>
2025-02-01 21:20:38 +0100

    SplashActivity base

---
+++ app/src/main/java/iut/dam/agapi/SplashActivity.java
@@ -0,0 +1,23 @@
+package iut.dam.agapi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
