e01d3efb052c08e54f5b9247d20699602ba55b1f
BaZz Ychnightder <pierreychnightder561@gmail.com>
2025-02-08 17:54:20 +0100

    à l'aide les constraintlayout j'arrive pas

---
+++ app/src/main/java/iut/dam/agapi/AssociationActivity.java
@@ -0,0 +1,15 @@
+package iut.dam.agapi;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AssociationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_association);
+
+    }
+}
