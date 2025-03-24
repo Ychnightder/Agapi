1f651163a6aeac3f3653b2134fea58c65acbbe86
TYGH222 <zhongqisu@gmail.com>
2025-03-24 16:09:11 +0100

    Supprime les page similaire(login/inscription) remplace par fragments

---
+++ app/src/main/java/fragments/InscriptionFragment.java
@@ -0,0 +1,22 @@
+package fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import iut.dam.newagapi_version311_clean.R;
+
+
+public class InscriptionFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_inscription, container, false);
+    }
+}
---
+++ app/src/main/java/iut/dam/newagapi_version311_clean/ui/Login_Inscription.java
@@ -0,0 +1,49 @@
+package iut.dam.newagapi_version311_clean.ui;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import fragments.InscriptionFragment;
+import fragments.LoginFragment;
+import iut.dam.newagapi_version311_clean.R;
+
+public class Login_Inscription extends AppCompatActivity {
+    Button buttonInscription, buttonConnection;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.testlogin);
+        buttonInscription = findViewById(R.id.titleInscription);
+        buttonConnection = findViewById(R.id.titlebuttonConnexion);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frameContainer, new LoginFragment())
+                .commit();
+
+        buttonInscription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("DEBUG", "Inscription button clicked!");
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.frameContainer, new InscriptionFragment())
+                        .commit();
+            }
+        });
+
+        buttonConnection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("DEBUG", "Inscription button clicked!");
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.frameContainer, new LoginFragment())
+                        .commit();
+            }
+        });
+
+    }
+}
